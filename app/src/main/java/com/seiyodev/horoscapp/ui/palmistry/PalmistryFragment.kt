95f196a252d2a040300d0cd03d58701bceb68389
Sergio Serrano <sergioserrano7@gmail.com>
2024-08-14 11:00:14 +0200

    Permissions camera added

--- app/src/main/java/com/seiyodev/horoscapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/seiyodev/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import com.seiyodev.horoscapp.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,8 +14,33 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class PalmistryFragment : Fragment() {
 
+    companion object {
+        private const val CAMERA_PERMISSION = android.Manifest.permission.CAMERA
+    }
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){ isGranted ->
+        if(isGranted){
+            //startCamera
+        } else {
+            Toast.makeText(requireContext(), "Debes de aceptar los permisos de cámara para poder utilizar este apartado :)", Toast.LENGTH_LONG).show()
+
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if(checkCameraPermission()){
+            //Tiene permisos aceptados
+            //Start Camera
+        } else {
+            requestPermissionLauncher.launch(CAMERA_PERMISSION)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +49,10 @@ class PalmistryFragment : Fragment() {
         _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    private fun checkCameraPermission():Boolean{
+       return PermissionChecker.checkSelfPermission(requireContext(), CAMERA_PERMISSION) == PermissionChecker.PERMISSION_GRANTED
     }
 
 }
